3ac56cca05240636e1c0b10268bba16ecb1af13e
Taikunon <96242268+Taikunon@users.noreply.github.com>
2022-07-27 21:00:56 +0200

    javaFX

--- GCMClient/src/gcmClient/ClientFXMain.java
+++ GCMClient/src/gcmClient/ClientFXMain.java
@@ -3,18 +3,38 @@ package gcmClient;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 
 public class ClientFXMain extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		BorderPane bp = new BorderPane();
+		
+		
+		VBox vb = new VBox(); // sidebar menu left side		
+		vb.setId("vb-bgcolor");
+		vb.getStyleClass().add("vb-bgcolor");
+		
+		
+		Label logoTxt = new Label("GCM");
+		logoTxt.setId("logo-txt");
+		logoTxt.getStyleClass().add("logo-txt");
+		
+		vb.getChildren().add(logoTxt);
+		vb.setPrefWidth(200);
+		vb.setPrefHeight(600);
+		AnchorPane ap = new AnchorPane(vb);	
+		
+		ap.setLeftAnchor(vb, 0.0);
 		
 
 		primaryStage.setTitle("Gaming Clan Manager");
-		primaryStage.setScene(new Scene(bp, 800, 600));
+		Scene scene = new Scene(ap, 900, 600);
+		primaryStage.setScene(scene);
+		scene.getStylesheets().add("style.css");
 		primaryStage.show();
 
 	}

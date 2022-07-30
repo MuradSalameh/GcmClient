f46a2d8f8dbca8e244bb3a16d4e29403c9ca0ea6
Taikunon <96242268+Taikunon@users.noreply.github.com>
2022-07-30 10:48:59 +0200

    JavaFX  Screen switcher completed

--- GCMClient/src/gcmClient/FXMain.java
+++ GCMClient/src/gcmClient/FXMain.java
@@ -25,5 +25,4 @@ public class FXMain extends Application {
 		launch(args);
 	}
 
-
 }
--- GCMClient/src/gcmClient/FxmlDocumentController.java
+++ GCMClient/src/gcmClient/FxmlDocumentController.java
@@ -13,7 +13,8 @@ import javafx.scene.layout.Pane;
 
 
 
-public class FxmlDocumentController implements Initializable {
+public class FxmlDocumentController implements Initializable {	
+	
 	@FXML
 	private Label label;
 	
@@ -29,6 +30,83 @@ public class FxmlDocumentController implements Initializable {
 		
 		bpScreenView.setLeft(view);		
 		System.out.println("Home Button klicked");
+		
+	}
+	@FXML
+	private void handleMembersBtn(ActionEvent event) throws IOException {
+		
+		FxmlLoader screen = new FxmlLoader();
+		Pane view = screen.getPage("MembersScreen");
+		System.out.println("Pane view: " + view);
+		
+		bpScreenView.setLeft(view);		
+		System.out.println("HoMembersme Button klicked");
+		
+	}
+	@FXML
+	private void handleGamesBtn(ActionEvent event) throws IOException {
+		
+		FxmlLoader screen = new FxmlLoader();
+		Pane view = screen.getPage("GamesScreen");
+		System.out.println("Pane view: " + view);
+		
+		bpScreenView.setLeft(view);		
+		System.out.println("Games Button klicked");
+		
+	}
+	@FXML
+	private void handleEventsBtn(ActionEvent event) throws IOException {
+		
+		FxmlLoader screen = new FxmlLoader();
+		Pane view = screen.getPage("EventsScreen");
+		System.out.println("Pane view: " + view);
+		
+		bpScreenView.setLeft(view);		
+		System.out.println("Events Button klicked");
+		
+	}
+	@FXML
+	private void handleTeamsBtn(ActionEvent event) throws IOException {
+		
+		FxmlLoader screen = new FxmlLoader();
+		Pane view = screen.getPage("TeamsScreen");
+		System.out.println("Pane view: " + view);
+		
+		bpScreenView.setLeft(view);		
+		System.out.println("Teams Button klicked");
+		
+	}
+	@FXML
+	private void handleTournamentsBtn(ActionEvent event) throws IOException {
+		
+		FxmlLoader screen = new FxmlLoader();
+		Pane view = screen.getPage("TournamentsScreen");
+		System.out.println("Pane view: " + view);
+		
+		bpScreenView.setLeft(view);		
+		System.out.println("Tournaments Button klicked");
+		
+	}
+	@FXML
+	private void handlePartnersBtn(ActionEvent event) throws IOException {
+		
+		FxmlLoader screen = new FxmlLoader();
+		Pane view = screen.getPage("PartnersScreen");
+		System.out.println("Pane view: " + view);
+		
+		bpScreenView.setLeft(view);		
+		System.out.println("Partners Button klicked");
+		
+	}
+	@FXML
+	private void handleFinancesBtn(ActionEvent event) throws IOException {
+		
+		FxmlLoader screen = new FxmlLoader();
+		Pane view = screen.getPage("FinancesScreen");
+		System.out.println("Pane view: " + view);
+		
+		bpScreenView.setLeft(view);		
+		System.out.println("Finances Button klicked");
 		
 	}
 
